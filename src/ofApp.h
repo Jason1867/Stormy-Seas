bb79fd9116192d032f2083df13bfd4cee8ec5d71
davthesim <164102545+davthesim@users.noreply.github.com>
2025-12-09 19:31:04 -0500

    Add Cloud structure and related functions

--- src/ofApp.h
+++ src/ofApp.h
@@ -22,6 +22,19 @@ public:
 	void gotMessage(ofMessage msg);
 
 private:
+	struct Cloud { // Cloud structure (for generating multiple).
+		ofMesh mesh; // Mesh of cloud.
+		glm::vec3 pos; // Position of cloud.
+		float size; // Size of cloud.
+	};
+
+	void build_Cloud(Cloud& cloud, const glm::vec3& pos, float size); // Builds the cloud.
+
+	vector<Cloud> clouds; // List of clouds.
+	ofShader cloudShader; // Cloud Shader.
+	glm::vec3 cloudColor; // Cloud Color.
+
+
 	float gerstnerWave(glm::vec3 pos, float t);
 
 	// Mesh and grid
